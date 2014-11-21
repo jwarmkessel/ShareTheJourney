75e8cb14ffba05e5dd2b4341e4b7d94e6af91f60
Justin Warmkessel <justin.w@ymedialabs.com>
2014-11-21 09:37:54 -0800

    Began mood survey

---
+++ BreastCancer/TasksAndSteps/APHMoodSurveyTaskViewController.h
@@ -0,0 +1,14 @@
+//
+//  APHMoodSurveyTaskViewController.h
+//  BreastCancer
+//
+//  Created by Justin Warmkessel on 11/20/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+@import APCAppleCore;
+#import <Foundation/Foundation.h>
+
+@interface APHMoodSurveyTaskViewController : APCBaseWithProgressTaskViewController
+
+@end
