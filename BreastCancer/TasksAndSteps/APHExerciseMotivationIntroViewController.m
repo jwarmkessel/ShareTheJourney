21ae72127652ef000defa6e98f19e49ed4f63aab
Justin Warmkessel <justin.w@ymedialabs.com>
2014-11-24 15:30:30 -0800

    Added question selected gesture and completion ui

--- BreastCancer/TasksAndSteps/APHExerciseMotivationIntroViewController.m
+++ BreastCancer/TasksAndSteps/APHExerciseMotivationIntroViewController.m
@@ -11,6 +11,17 @@
 @interface APHExerciseMotivationIntroViewController ()
 - (IBAction)nextButtonTapped:(id)sender;
 
+@property (weak, nonatomic) IBOutlet UILabel *walkingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *exerciseLabel;
+@property (weak, nonatomic) IBOutlet UILabel *stepsLabel;
+
+@property (weak, nonatomic) IBOutlet APCConfirmationView *walkingSelectedView;
+@property (weak, nonatomic) IBOutlet APCConfirmationView *exerciseSelectedView;
+@property (weak, nonatomic) IBOutlet APCConfirmationView *stepsSelectedView;
+
+
+
+@property (nonatomic, strong) NSString *selectedGoal;
 @end
 
 @implementation APHExerciseMotivationIntroViewController
@@ -20,6 +31,80 @@
     [self.view setBackgroundColor:[UIColor appSecondaryColor4]];
     
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelButtonTapped:)];
+    
+    NSArray *buttons = @[self.walkingLabel, self.exerciseLabel, self.stepsLabel];
+    NSArray *selectedViews = @[self.walkingSelectedView, self.exerciseSelectedView, self.stepsSelectedView];
+    
+    for (int i = 0; i<[buttons count]; i++) {
+        UILabel *label = (UILabel *) buttons[i];
+        APCConfirmationView *selectedView = (APCConfirmationView *)selectedViews[i];
+        
+        label.tag = i + 1;
+        
+        
+        UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(oneTap:)];
+        singleTap.numberOfTapsRequired = 1;
+        [label addGestureRecognizer:singleTap];
+        
+        [label setUserInteractionEnabled:YES];
+        
+        UITapGestureRecognizer *singleTapSelected = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(oneTap:)];
+        singleTapSelected.numberOfTapsRequired = 1;
+        [selectedView addGestureRecognizer:singleTapSelected];
+        [selectedView setUserInteractionEnabled:YES];
+        
+        selectedView.tag = i + 1;
+    }
+}
+
+- (void)oneTap:(UIGestureRecognizer *)gesture {
+    NSInteger selectedViewTag = gesture.view.tag;
+    UILabel *selectedLabel = (UILabel *) [self.view viewWithTag:selectedViewTag];
+    self.selectedGoal = selectedLabel.text;
+    NSLog(@"%@", selectedLabel.text);
+    
+    NSArray *selectedViews = @[self.walkingSelectedView, self.exerciseSelectedView, self.stepsSelectedView];
+    
+    for (APCConfirmationView *view in selectedViews) {
+        [view setCompleted:NO];
+    }
+    
+    APCConfirmationView *selectedView;
+    selectedView.alpha = 0.3;
+    
+    switch (selectedViewTag)
+    
+    {
+        case 1:
+            
+            selectedView = self.walkingSelectedView;
+            [selectedView setAlpha:0];
+            break;
+            
+        case 2:
+            
+            selectedView = self.exerciseSelectedView;
+            [selectedView setAlpha:0];
+            
+            break;
+
+        case 3:
+            
+            selectedView = self.stepsSelectedView;
+            [selectedView setAlpha:0];
+            
+            break;
+            
+
+        default:
+            
+            break;
+    }
+    
+    [selectedView setCompleted:YES];
+    [UIView animateWithDuration:0.5 animations:^{
+        [selectedView setAlpha:1];
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
