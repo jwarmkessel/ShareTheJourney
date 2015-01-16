d3f4abc0266af4599bedcc569da81189c2864a5c
Justin Warmkessel <justin.w@ymedialabs.com>
2015-01-15 16:00:58 -0800

    Removed activities info from dashboard.

--- BreastCancer/TabBarControllers/Dashboard/APHDashboardViewController.m
+++ BreastCancer/TabBarControllers/Dashboard/APHDashboardViewController.m
@@ -149,23 +149,7 @@ static NSString * const kAPCRightDetailTableViewCellIdentifier = @"APCRightDetai
         
         NSUInteger allScheduledTasks = ((APCAppDelegate *)[UIApplication sharedApplication].delegate).dataSubstrate.countOfAllScheduledTasksForToday;
         NSUInteger completedScheduledTasks = ((APCAppDelegate *)[UIApplication sharedApplication].delegate).dataSubstrate.countOfCompletedScheduledTasksForToday;
-        
-        {
-            APCTableViewItem *item = [APCTableViewItem new];
-            item.caption = NSLocalizedString(@"Activities", @"");
-            item.identifier = kAPCRightDetailTableViewCellIdentifier;
-            item.editable = NO;
-            item.textAlignnment = NSTextAlignmentRight;
-            
-            
-            item.detailText = [NSString stringWithFormat:@"%lu/%lu", (unsigned long)completedScheduledTasks, (unsigned long)allScheduledTasks];
-            
-            APCTableViewRow *row = [APCTableViewRow new];
-            row.item = item;
-            row.itemType = kAPCTableViewDashboardItemTypeProgress;
-            [rowItems addObject:row];
-        }
-        
+                
         {
             APCTableViewDashboardProgressItem *item = [APCTableViewDashboardProgressItem new];
             item.identifier = kAPCDashboardProgressTableViewCellIdentifier;
