ff7f714088d48e20a9d113f28afc096613656bbc
David Barnard <david.barnard@ymedialabs.com>
2015-02-12 00:21:19 -0500

    I'm not exactly sure how to create test data for this part.  But, the idea is that the entries are sectioned based on their week of year and year.  The log is sorted before sectioning and sections are added to an array instead of set to preserve that ordering.  Made the header view a new table view cell subclass and registered it to the table view.  The first section does not display this new header.  It instead displays the previously implemented header: instructions.  Also had to refactor places where accessing logHistory array was used.

---
+++ BreastCancer/TasksAndSteps/DailyJournalControllers/APHNotesHeaderTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  APHNotesHeaderTableViewCell.h
+//  BreastCancer
+//
+//  Created by David Barnard on 2/11/15.
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APHNotesHeaderTableViewCell : UITableViewCell
+@property (weak) IBOutlet UILabel *labelWeek;
+@property (weak) IBOutlet UILabel *labelEntries;
+@end
---
+++ BreastCancer/TasksAndSteps/DailyJournalControllers/APHNotesHeaderTableViewCell.m
@@ -0,0 +1,20 @@
+//
+//  APHNotesHeaderTableViewCell.m
+//  BreastCancer
+//
+//  Created by David Barnard on 2/11/15.
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import "APHNotesHeaderTableViewCell.h"
+
+@implementation APHNotesHeaderTableViewCell
+
+- (void)awakeFromNib {
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
