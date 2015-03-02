a4abcb49e00b513636ed7c894c9dd476e485008d
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2015-03-02 07:31:46 -0800

    Adding surveys
    Updating AppCore submodule.

---
+++ BreastCancer/TasksAndSteps/Generic Surveys/APHPsqiSurveyViewController.m
@@ -0,0 +1,37 @@
+//
+//  APHPsqiSurveyViewController.m
+//  BreastCancer
+//
+//  Created by Farhan Ahmed on 3/2/15.
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import "APHPsqiSurveyViewController.h"
+
+@interface APHPsqiSurveyViewController ()
+
+@end
+
+@implementation APHPsqiSurveyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
